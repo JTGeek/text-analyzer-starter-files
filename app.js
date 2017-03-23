73985f2853d2b78036f160a697bed1863ac0ede4
James Timberlake <James.Timberlake.1018@gmail.com>
2017-03-23 10:25:50 -0400

    draft one

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-// your code here!
+function formatText(text) {
+    return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
+}
+
+
+function removeReturns(text) {
+    return text.replace(/\r?\n|\r/g, "");
+}
+
+
+
+
+
+function reportOnText(text) {
+
+    var formatedText = uniqueWordCount(formatText(text));
+    var numDistinctWords = uniqueWordCount;
+    var numTotalWords = wordCount
+    var averageWordLength = avgWordCount;
+
+
+
+
+    // take our data and display it in the dom
+    var textReport = $('.js-text-report');
+    textReport.find('.js-word-count').text(numTotalWords);
+    textReport.find('.js-unique-word-count').text(numDistinctWords);
+    textReport.find('.js-average-word-length').text(
+        averageWordLength + " characters");
+    textReport.removeClass('hidden');
+}
+
+
+function uniqueWordCount(words) {
+    var unique = {};
+    var total = 0;
+
+    for (var word of words) {
+        if (!unique[word]) {
+            unique[word] = 1;
+        }
+        total += word.length;
+    }
+
+
+    return {
+        wordCount: words.length,
+        unique: Object.keys(unique).length,
+        avgWordCount: total / words.length
+    };
+}
+
+
+function watchFormSubmission() {
+    $('.js-text-form').submit(function (event) {
+        event.preventDefault();
+        // get the text the user submitted
+        var userText = $(this).find('#user-text').val();
+        reportOnText(removeReturns(userText));
+    });
+}
+
+
+
+watchFormSubmission();
